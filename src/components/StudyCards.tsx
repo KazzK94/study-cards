80bf19074699a06f75cf20abc8c2e4a45b951c26
Kazz <i94773007@gmail.com>
2025-02-15 20:19:12 +0100

    slightly improved UX for study cards

--- src/components/StudyCards.tsx
+++ src/components/StudyCards.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { getStudyDecks } from '../data/storage'
 import { StudyDeck } from '../types'
+import { ArrowLeftIcon, ArrowRightIcon, ListRestartIcon } from 'lucide-react'
 
 export function StudyCards() {
 	const { blockId } = useParams<{ blockId: string }>()
@@ -45,6 +46,11 @@ export function StudyCards() {
 		}
 	}
 
+	const restart = () => {
+		setCurrentCardIndex(0)
+		setShowAnswer(false)
+	}
+
 	return (
 		<div className="text-center">
 			<h2 className="text-2xl font-bold mb-4">{deck.title}</h2>
@@ -52,9 +58,9 @@ export function StudyCards() {
 				<button
 					onClick={prevCard}
 					disabled={currentCardIndex === 0}
-					className="bg-gray-300 text-black px-4 py-2 rounded hover:opacity-90 disabled:opacity-50 cursor-pointer"
+					className="bg-gray-300 text-gray-800/90 disabled:bg-gray-200/80 disabled:text-gray-300 px-4 py-2 rounded cursor-pointer"
 				>
-					Previous
+					<ArrowLeftIcon size={24} />
 				</button>
 				<p>
 					{currentCardIndex + 1} / {deck.cards.length}
@@ -62,21 +68,43 @@ export function StudyCards() {
 				<button
 					onClick={nextCard}
 					disabled={currentCardIndex === deck.cards.length - 1}
-					className="bg-gray-300 text-black px-4 py-2 rounded hover:opacity-90 disabled:opacity-50 cursor-pointer"
+					className="bg-gray-300 text-gray-800/90 disabled:bg-gray-200/80 disabled:text-gray-300 px-4 py-2 rounded cursor-pointer"
 				>
-					Next
+					<ArrowRightIcon size={24} />
 				</button>
 			</div>
-			<div className="bg-white p-6 rounded shadow-md mb-4 w-[90%] max-w-3xl mx-auto">
-				<div className="text-xl mb-4 space-y-3">
-					<div className='text-blue-800'>
+			<div className="bg-white px-6 pt-6 pb-4 rounded shadow-md mb-4 w-[90%] max-w-3xl mx-auto">
+				<div className="text-xl mb-4 space-y-4">
+					<div className='text-blue-800 font-semibold'>
 						{currentCard.front.split('\n').map((line, i) => (<p key={i}>{line}</p>))}
 					</div>
 					{
 						showAnswer && (
-							<div className='text-green-700'>
-								{currentCard.back.split('\n').map((line, i) => (<p key={i}>{line}</p>))}
-							</div>
+							<>
+								<div className='text-green-700 border-t border-gray-200 pt-4'>
+									{currentCard.back.split('\n').map((line, i) => (<p key={i}>{line}</p>))}
+								</div>
+								{
+									(currentCardIndex !== deck.cards.length - 1)
+										? (
+											<button
+												onClick={nextCard}
+												className='flex justify-center items-center gap-2 mx-auto border rounded-lg px-3 py-2 mt-8 bg-blue-700/90 text-white cursor-pointer text-base'
+											>
+												See Next Card
+												<ArrowRightIcon size={20} />
+											</button>
+										) : (
+											<button
+												onClick={restart}
+												className='flex justify-center items-center gap-2 mx-auto border rounded-lg px-3 py-2 mt-8 bg-slate-800 text-white cursor-pointer text-base'
+											>
+												<ListRestartIcon size={20} />
+												Start Again
+											</button>
+										)
+								}
+							</>
 						)
 					}
 				</div>
